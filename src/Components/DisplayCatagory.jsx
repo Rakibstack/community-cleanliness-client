ad8c3860bf5537727b93ffdcdd57acc3f270e452
Rakibstack <rh6039511@gmail.com>
2025-11-09 19:06:14 +0600

    added display catagory section and make them responsive

--- src/Components/DisplayCatagory.jsx
+++ src/Components/DisplayCatagory.jsx
@@ -2,31 +2,48 @@ import React from 'react';
 
 const DisplayCatagory = () => {
     return (
-        <div className='py-14 bg-gray-100'>
+        <div className='py-14 bg-base-100'>
             <div>
-                 <h2 className='text-center text-4xl font-bold mb-4'>Category Section</h2>
+                <h2 className='text-center text-4xl font-bold mb-4'>Category Section</h2>
 
             </div>
-            <div className='container mx-auto w-11/12 grid grid-cols-4 gap-4 p-5'>
-             
+            <div className='container mx-auto w-11/12 grid sm:grid-cols-2 md:grid-cols-2  lg:grid-cols-4 gap-4 p-5'>
+
                 <div className='bg-white p-5 rounded-2xl shadow-2xl'>
-                    <h2 className='text-2xl'>Title</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat qui inventore unde dolores sit ipsam magni enim optio voluptatum pariatur tenetur adipisci temporibus ad, accusantium quod ratione commodi, porro accusamus.</p>
+                    <h2 className='text-2xl font-bold text-gray-600 '>Garbage</h2>
+                    <p className='tracking-wider text-base-700'>Improperly disposed waste causing environmental pollution.
+
+                        Accumulated trash affecting public health and cleanliness.
+
+                        Unmanaged waste leading to foul odor and hygiene issues.
+                    </p>
 
                 </div>
                 <div className='bg-white p-5 rounded-2xl shadow-2xl'>
-                    <h2 className='text-2xl'>Title</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat qui inventore unde dolores sit ipsam magni enim optio voluptatum pariatur tenetur adipisci temporibus ad, accusantium quod ratione commodi, porro accusamus.</p>
+                    <h2 className='text-2xl font-bold text-gray-600'>Illegal Construction </h2>
+                    <p className='tracking-wider text-base-700'>Unauthorized building work violating city regulations.
+
+                        Structures built without proper approval or permits.
+
+                        Illegally extended property posing safety risks.</p>
 
                 </div>
                 <div className='bg-white p-5 rounded-2xl shadow-2xl'>
-                    <h2 className='text-2xl'>Title</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat qui inventore unde dolores sit ipsam magni enim optio voluptatum pariatur tenetur adipisci temporibus ad, accusantium quod ratione commodi, porro accusamus.</p>
+                    <h2 className='text-2xl font-bold text-gray-600'>Broken Public Property</h2>
+                    <p className='tracking-wider text-base-700'>Damaged public assets affecting community use.
+
+                        Broken facilities requiring urgent repair.
+
+                        Vandalized or worn-out public structures.</p>
 
                 </div>
                 <div className='bg-white p-5 rounded-2xl shadow-2xl'>
-                    <h2 className='text-2xl'>Title</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat qui inventore unde dolores sit ipsam magni enim optio voluptatum pariatur tenetur adipisci temporibus ad, accusantium quod ratione commodi, porro accusamus.</p>
+                    <h2 className='text-2xl font-bold text-gray-600'>Road Damag</h2>
+                    <p className='tracking-wider text-base-700'>Cracked or uneven roads slowing traffic movement.
+
+                        Potholes creating vehicle safety hazards.
+
+                        Damaged road surfaces due to poor maintenance.</p>
 
                 </div>
 
